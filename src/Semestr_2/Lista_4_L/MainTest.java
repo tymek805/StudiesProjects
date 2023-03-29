b0eab1e223bc2e745b7007b2619119dbc302f684
Tymoteusz <tymoteusz.lango@gmail.com>
2023-03-29 21:09:52 +0200

    Modification of list 4

--- src/Semestr_2/Lista_4_L/MainTest.java
+++ src/Semestr_2/Lista_4_L/MainTest.java
@@ -8,6 +8,17 @@ public class MainTest {
         list.add(4);
         list.add(2);
         list.add(3);
+        list.add(7);
+        list.add(5);
+        list.add(6);
+        System.out.println("Size: " + list.size());
+        System.out.println("Empty: " + list.isEmpty());
 
+        list.add(5, 10);
+
+        while (!list.isEmpty())
+            System.out.println(list.remove(0));
+        System.out.println("Size: " + list.size());
+        System.out.println("Empty: " + list.isEmpty());
     }
 }
