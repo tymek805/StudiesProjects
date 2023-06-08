12ae9eb9b5677ed134d915acd60adee63fbd1cf2
Tymoteusz <tymoteusz.lango@op.pl>
2023-06-08 08:26:48 +0200

    Lista 10

---
+++ src/Semestr_2/Lista_10_L/Lista/ForestNode.java
@@ -0,0 +1,17 @@
+package Semestr_2.Lista_10_L.Lista;
+
+public class ForestNode {
+    private ForestNode parent;
+    private int rank;
+
+    public ForestNode() {
+        rank = 0;
+        parent = this;
+    }
+
+    public ForestNode getParent() {return parent;}
+    public void setParent(ForestNode parent) {this.parent = parent;}
+
+    public int getRank() {return rank;}
+    public void setRank(int rank) {this.rank = rank;}
+}
---
+++ src/Semestr_2/Lista_10_L/Lista/ListDisjointSet.java
@@ -0,0 +1,38 @@
+package Semestr_2.Lista_10_L.Lista;
+
+public class ListDisjointSet implements DisjointSet<Node>{
+    @Override
+    public Node makeSet() {
+        return new Node();
+    }
+
+    @Override
+    public boolean union(Node x, Node y) {
+        Node rootX = findSet(x);
+        Node rootY = findSet(y);
+        if (rootX == rootY)
+            return false;
+
+        if (rootX.getLength() <= rootY.getLength()) join(rootX, rootY);
+        else join(rootY, rootX);
+
+        return true;
+    }
+
+    private void join (Node rootX, Node rootY) {
+        rootX.getLast().setNext(rootY);
+        rootX.setLast(rootY.getLast());
+        rootX.setLength(rootX.getLength() + rootY.getLength());
+
+        Node node = rootY;
+        while (node != null){
+            node.setRepresentative(rootX);
+            node = node.getNext();
+        }
+    }
+
+    @Override
+    public Node findSet(Node node) {
+        return node.getRepresentative();
+    }
+}
