7440f235e60d64cfb9b5cf2180e1735ad3764783
tymek805 <tymoteusz.lango@gmail.com>
2023-11-18 00:42:48 +0100

    Add second and third task. Refactor to follow naming convention

--- List_1/solution.cpp
+++ List_1/solution.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "solution.h"
 
-void alloc_table_fill_34(int size) {
+void allocTableFill34(int size) {
     while (size <= 0) {
         std::cout << "Set value again: ";
         std::cin >> size;
@@ -16,4 +16,31 @@ void alloc_table_fill_34(int size) {
         std::cout << table[i] << ' ';
 
     delete[] table;
+}
+
+bool allocTable2Dim(int*** table, int sizeX, int sizeY) {
+    if (table == NULL || sizeX <= 0 || sizeY <= 0)
+        return false;
+
+    int** twoDimensionalTable = new int* [sizeX];
+    for (int i = 0; i < sizeX; i++)
+        twoDimensionalTable[i] = new int[sizeY];
+
+    deallocTable2Dim(table, sizeX, sizeY);
+    *table = twoDimensionalTable;
+
+    return true;
+}
+
+bool deallocTable2Dim(int*** table, int sizeX, int sizeY) {
+    if (table == NULL || sizeX <= 0 || sizeY <= 0)
+        return false;
+
+    int** twoDimensionalTable = *table;
+
+    for (int i = 0; i < sizeX; i++)
+        delete twoDimensionalTable[i];
+
+    delete twoDimensionalTable;
+    return true;
 }
--- List_1/solution.h
+++ List_1/solution.h
@@ -1,5 +1,5 @@
-void alloc_table_fill_34(int size);
-bool alloc_table_2_dim(int*** table, int sizeX, int sizeY);
-bool dealloc_table_2_dim(int*** table, int sizeX, int sizeY);
+void allocTableFill34(int size);
+bool allocTable2Dim(int*** table, int sizeX, int sizeY);
+bool deallocTable2Dim(int*** table, int sizeX, int sizeY);
 
 const int FILL_VALUE = 34;
