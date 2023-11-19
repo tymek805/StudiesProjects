f838b2aaf25772c7a12c885fb4864a788f9f19ad
tymek805 <tymoteusz.lango@gmail.com>
2023-11-19 21:15:28 +0100

    Adds implementations of the Node class functions

--- List_3/Tree/Node.cpp
+++ List_3/Tree/Node.cpp
@@ -1,1 +1,48 @@
 #include "Node.h"
+
+Node::Node() {
+    value = DEFAULT_FIX_VALUE;
+    left = nullptr;
+    right = nullptr;
+}
+
+Node::Node(const std::string& value) {
+    this->value = value;
+    left = nullptr;
+    right = nullptr;
+}
+
+Node::Node(const std::string& value, Node *left, Node *right) {
+    this->value = value;
+    this->left = left;
+    this->right = right;
+}
+
+Node::~Node() {
+    delete left;
+    delete right;
+}
+
+std::string Node::getValue() {
+    return value;
+}
+Node *Node::getLeft() {
+    return left;
+}
+Node *Node::getRight() {
+    return right;
+}
+
+void Node::setValue(const std::string& newValue) {
+    value = newValue;
+}
+
+void Node::setLeft(Node *newLeft) {
+    delete left;
+    left = newLeft;
+}
+
+void Node::setRight(Node *newRight) {
+    delete right;
+    right = newRight;
+}
--- List_3/Tree/Node.h
+++ List_3/Tree/Node.h
@@ -1,3 +1,22 @@
-class Node {
+#include <string>
+#define DEFAULT_FIX_VALUE "1"
 
+class Node {
+public:
+    Node();
+    Node(const std::string &value);
+    Node(const std::string &value, Node* left, Node* right);
+    ~Node();
+
+    std::string getValue();
+    Node* getLeft();
+    Node* getRight();
+
+    void setValue(const std::string& newValue);
+    void setLeft(Node* newLeft);
+    void setRight(Node* newRight);
+private:
+    std::string value;
+    Node* left;
+    Node* right;
 };
