2090054bb2490425cb3b1748e4e48bcc5c85661a
Tymoteusz Lango <58483383+tymek805@users.noreply.github.com>
2024-02-19 00:44:21 +0100

    Add second exercise

---
+++ src/Semestr_2/Computer architecture/List_2/zad1.asm
@@ -0,0 +1,26 @@
+.data
+prompt: .asciiz "Podaj liczbe (0-9): "
+result: .asciiz "Reszta z dzielenia przez 4: "
+
+.text
+.globl main
+
+main:
+    li $v0, 4
+    la $a0, prompt
+    syscall
+    
+    li $v0, 5
+    syscall
+    
+    move $t0, $v0
+    
+    andi $t1, $t0, 0x3
+    
+    li $v0, 4
+    la $a0, result
+    syscall
+    
+    move $a0, $t1
+    li $v0, 1
+    syscall
