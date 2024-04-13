477077696e5f1607e5d64a1455025a02505b010c
tymek805 <tymoteusz.lango@op.pl>
2024-04-13 12:06:13 +0200

    Exercise 3 from fourth exercise list

---
+++ src/Semestr_4/ScriptingLanguages/List_4/head.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def head():
+    args = sys.argv[1:]
+    bytes_to_read = 10
+    show_filename = False
+
+    for arg in sys.argv[1:]:
+        if arg.startswith("--bytes="):
+            bytes_to_read = int(arg.split('=')[-1])
+            args.remove(arg)
+        elif arg == "-v":
+            show_filename = True
+            args.remove(arg)
+
+    if len(args) == 0:
+        filename = "<stdin>"
+        data = sys.stdin.read(bytes_to_read)
+    else:
+        filename = args[0]
+        with open(filename, 'rb') as f:
+            data = f.read(bytes_to_read)
+
+    if show_filename:
+        print(filename)
+    print(data)
+
+
+if __name__ == "__main__":
+    head()
