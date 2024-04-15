faf0b3202b17c64d87029faeb5adc6078cc557b6
tymek805 <tymoteusz.lango@op.pl>
2024-04-15 11:57:46 +0200

    Add subprocess script

---
+++ src/Semestr_4/ScriptingLanguages/List_4/run_subprocess.py
@@ -0,0 +1,32 @@
+import json
+import os
+import subprocess
+import sys
+
+from file_statistics import analyze_file
+
+
+def run():
+    dir_path = sys.argv[1]
+
+    results = []
+
+    for filename in os.listdir(dir_path):
+        if os.path.isfile(filename):
+            filepath = os.path.join(dir_path, filename)
+            process = subprocess.Popen(["python3", "file_statistics.py"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
+            results.append(json.loads(process.communicate(input=filepath)[0]))
+
+    print({
+        'read files': len(results),
+        'characters sum': sum(result['character count'] for result in results),
+        'words sum': sum(result['words count'] for result in results),
+        'rows sum': sum(result['row count'] for result in results),
+        'most frequent character': max(set(result["most frequent character"] for result in results), key=results.count),
+        'most frequent word': max(set(result["most frequent word"] for result in results), key=results.count),
+    })
+    # print(results)
+
+
+if __name__ == "__main__":
+    run()
