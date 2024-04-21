bf42110e8bf1ab65432acd377eee8240804efc55
tymek805 <tymoteusz.lango@op.pl>
2024-04-21 22:24:05 +0200

    Add logger

---
+++ src/Semestr_4/ScriptingLanguages/List_5/logger.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+
+
+def define_logger():
+    logger = logging.getLogger()
+    logging.basicConfig(
+        format="%(levelname)s: %(message)s",
+        level=logging.DEBUG,
+        filemode="w",
+        filename="logs.log",
+    )
+
+    fmt = logging.Formatter('%(levelname)s: %(message)s')
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.DEBUG)
+    stdout_handler.addFilter(lambda record: record.levelno <= logging.INFO)
+    stdout_handler.setFormatter(fmt)
+    logger.addHandler(stdout_handler)
+
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.ERROR)
+    stderr_handler.setFormatter(fmt)
+    logger.addHandler(stderr_handler)
+
+    return logger
