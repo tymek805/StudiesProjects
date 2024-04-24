80cc88fdff0b4e413e3c6c8b5a5b0f84abea23bd
tymek805 <tymoteusz.lango@op.pl>
2024-04-24 21:24:25 +0200

    Implemented regex for matching raw log

--- src/Semestr_4/ScriptingLanguages/List_6/SSHLogEntry.py
+++ src/Semestr_4/ScriptingLanguages/List_6/SSHLogEntry.py
@@ -3,12 +3,21 @@ from ipaddress import IPv4Address
 
 
 class SSHLogEntry:
-    def __init__(self, raw_log):
-        self.date = " ".join(raw_log[:3])
-        self.host = raw_log[3]
-        self.component = raw_log[4].split('[')[0]
-        self.pid = raw_log[4].split('[')[1].split(']')[0]
-        self.message = " ".join(raw_log[5:])
+    _log_pattern = re.compile(
+        r"(?P<timestamp>\w{3}\s{1,2}\d{1,2}\s\d{2}:\d{2}:\d{2})\s(?P<host>\w+)\s(?P<component>\w+)\[(?P<pid>\d+)]:\s(?P<message>.+)"
+    )
+
+    def __init__(self, raw_log: str):
+        match = re.match(self._log_pattern, raw_log)
+
+        if match:
+            self.date = match.group('timestamp')
+            self.host = match.group('host')
+            self.component = match.group('component')
+            self.pid = match.group('pid')
+            self.message = match.group('message')
+        else:
+            raise ValueError("Provided log could not be parsed")
 
     def __repr__(self):
         return (f"{self.__class__.__name__}( "
@@ -19,6 +28,15 @@ class SSHLogEntry:
                 f"Message: \"{self.message}\" "
                 f")")
 
-    def get_ipv4(self):
-        ipv4 = re.findall(r'[0-9]+(?:\.[0-9]+){3}', self.message)
+    def ipv4(self):
+        ipv4 = re.findall(r"[0-9]+(?:\.[0-9]+){3}", self.message)
         return IPv4Address(ipv4[0]) if ipv4 else None
+
+
+class FailedSSH(SSHLogEntry):
+    def __init__(self, raw_log):
+        super().__init__(raw_log)
+
+
+if __name__ == "__main__":
+    SSHLogEntry('Jan  7 16:56:15 LabSZ sshd[30216]: Failed password for root from 104.192.1.10 port 41994 ssh2')
