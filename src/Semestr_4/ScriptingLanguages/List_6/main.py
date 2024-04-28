e197c172db3f17096a53e13edbc22bc2fcf1a65f
tymek805 <tymoteusz.lango@op.pl>
2024-04-29 01:32:31 +0200

    Create better initializer scenario

--- src/Semestr_4/ScriptingLanguages/List_6/main.py
+++ src/Semestr_4/ScriptingLanguages/List_6/main.py
@@ -1,46 +1,31 @@
-import os
-import re
 from datetime import datetime
-from enum import Enum
 
-from SSHLogEntry import RejectedPasswordSSH, AcceptedPasswordSSH, ErrorSSH, OtherSSH
+from SSHLogEntry import OtherSSH
 from SSHLogJournal import SSHLogJournal
 from SSHUser import SSHUser
 
-
 if __name__ == "__main__":
     journal = SSHLogJournal()
-    x = RejectedPasswordSSH('Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2')
-    y = AcceptedPasswordSSH('Dec 10 09:32:20 LabSZ sshd[24680]: Accepted password for fztu from 119.137.62.142 port 49116 ssh2')
-    z = ErrorSSH('Dec 10 11:03:44 LabSZ sshd[25455]: error: Received disconnect from 103.99.0.122: 14: No more user authentication methods available. [preauth]')
-    [print(ssh.validate()) for ssh in [x, y, z]]
-    journal.append('Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2')
-    journal.append('Dec 10 09:32:20 LabSZ sshd[24680]: Accepted password for fztu from 119.137.62.142 port 49116 ssh2')
-    journal.append('Dec 10 11:03:44 LabSZ sshd[25455]: error: Received disconnect from 103.99.0.122: 14: No more user authentication methods available. [preauth]')
+    test_line = ''
+
+    with open('SSH.log', 'r') as f:
+        for line in f.readlines()[:10]:
+            if test_line == '': test_line = line
+            journal.append(line)
+
+    for line in journal:
+        print(line)
+    print(OtherSSH(test_line) in journal)
+    print('test SSH' in journal)
+    print(len(journal))
     print(journal.get_logs_by_date(datetime(1800, 1, 1)))
 
-    journal = SSHLogJournal()
-    journal.append("Dec 10 06:55:46 LabSZ sshd[24200]: reverse mapping checking getaddrinfo for ns.marryaldkfaczcz.com [173.234.31.186] failed - POSSIBLE BREAK-IN ATTEMPT!")
-    journal.append("Dec 10 06:55:46 LabSZ sshd[24200]: Invalid user webmaster from 173.234.31.186")
-    journal.append("Dec 10 06:55:46 LabSZ sshd[24200]: input_userauth_request: invalid user webmaster [preauth]")
-    journal.append("Dec 10 06:55:46 LabSZ sshd[24200]: pam_unix(sshd:auth): check pass; user unknown")
-    journal.append("Dec 10 06:55:46 LabSZ sshd[24200]: pam_unix(sshd:auth): authentication failure; logname= uid=0 euid=0 tty=ssh ruser= rhost=173.234.31.186")
-    journal.append("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2")
-    journal.append("Dec 10 06:55:48 LabSZ sshd[24200]: Connection closed by 173.234.31.186 [preauth]")
-    journal.append("Dec 10 07:02:47 LabSZ sshd[24203]: Connection closed by 212.47.254.145 [preauth]")
-    journal.append("Dec 10 07:07:38 LabSZ sshd[24206]: Invalid user test9 from 52.80.34.196")
-    journal.append("Dec 10 07:07:38 LabSZ sshd[24206]: input_userauth_request: invalid user test9 [preauth]")
-    journal.append("Dec 10 07:07:38 LabSZ sshd[24206]: pam_unix(sshd:auth): check pass; user unknown")
-    journal.append("Dec 10 07:07:38 LabSZ sshd[24206]: pam_unix(sshd:auth): authentication failure; logname= uid=0 euid=0 tty=ssh ruser= rhost=ec2-52-80-34-196.cn-north-1.compute.amazonaws.com.cn")
     users = [SSHUser("webmaster", None), SSHUser("test9", None)]
+    for user in users:
+        print(user.validate())
 
-    log_entries = journal.get_logs_by_date(datetime(1800, 1, 1))
-
-    two_lists = users + log_entries
+    two_lists = users + journal.get_logs_by_date(datetime(1800, 1, 1))
 
     for item in two_lists:
-        print(item)
-        if not item.validate():
-            print(f"Invalid item: {item}")
-            break
-
+        if item.validate():
+            print(item)
