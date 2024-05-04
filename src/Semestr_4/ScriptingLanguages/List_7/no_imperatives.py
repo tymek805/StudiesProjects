720694484973110f7b8a660020c4ca164223f239
tymek805 <tymoteusz.lango@op.pl>
2024-05-04 10:00:02 +0200

    Remove redundant functions and add required implementations

--- src/Semestr_4/ScriptingLanguages/List_7/no_imperatives.py
+++ src/Semestr_4/ScriptingLanguages/List_7/no_imperatives.py
@@ -1,11 +1,9 @@
-def acronyms(strings: list[str]) -> str:
-    return ''.join([string[0] for string in strings])
+def odwr(string: str) -> str:
+    return ' '.join(string.split()[::-1])
 
 
-def median(digits: list[int | float]) -> int | float:
-    digits.sort()
-    n = len(digits)
-    return digits[(n + 1) // 2 - 1] if n % 2 != 0 else (digits[n // 2 - 1] + digits[n // 2]) / 2
+def most_common_element(array: list):
+    return max(set(array), key=array.count)
 
 
 def pierwiastek(x: int | float, epsilon: int | float, y=-1.0) -> int | float:
@@ -15,7 +13,8 @@ def pierwiastek(x: int | float, epsilon: int | float, y=-1.0) -> int | float:
 
 def make_alpha_dict(arg: str) -> dict:
     ans_dict = {}
-    [[ans_dict.update({c: [word]}) if ans_dict.get(c) is None else ans_dict.get(c).append(word) for c in word] for word in arg.split() if word != '']
+    [[ans_dict.update({c: [word]}) if ans_dict.get(c) is None else ans_dict.get(c).append(word) for c in word] for word
+     in arg.split() if word != '']
     return ans_dict
 
 
@@ -27,8 +26,8 @@ def flatten(array: list) -> list:
 
 
 if __name__ == "__main__":
-    print(acronyms(["Zakład", "Ubezpieczeń", "Społecznych"]))
-    print(median([1, 1, 19, 2, 3, 4, 4, 5, 1]))
+    print(odwr("To jest przykładowe zdanie do odwrócenia"))
+    print(most_common_element([1, 1, 19, 2, 3, 4, 4, 5, 1]))
     print(pierwiastek(3, epsilon=0.1))
     print(make_alpha_dict("on i ona"))
     print(flatten([1, [2, 3], [[4, 5], 6]]))
