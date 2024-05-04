2d9b5914949bc6e075f5cdfe0a866eeb4f7ff529
tymek805 <tymoteusz.lango@op.pl>
2024-05-04 10:35:53 +0200

    Add memory generator for 7th exercise list

--- src/Semestr_4/ScriptingLanguages/List_7/generator.py
+++ src/Semestr_4/ScriptingLanguages/List_7/generator.py
@@ -1,3 +1,6 @@
+from functools import cache
+
+
 def make_generator(function):
     def generator():
         n = 1
@@ -16,8 +19,18 @@ def fibonacci(n: int):
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
+def make_generator_mem(function):
+    @cache
+    def cached_f(n):
+        return function(n)
+
+    return make_generator(cached_f)
+
+
 if __name__ == "__main__":
     # print("Generator with Fibonacci: ")
     # [print(value) for value in make_generator(fibonacci)()]
-    print("Power sequence generator: ")
-    [print(value) for value in make_generator(lambda x: x ** 2)()]
+    # print("Power sequence generator: ")
+    # [print(value) for value in make_generator(lambda x: x ** 2)()]
+    print("Power sequence generator_mem: ")
+    [print(value) for value in make_generator_mem(lambda x: x ** 2)()]
