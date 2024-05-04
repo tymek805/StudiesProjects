c2bbf71e543a3e3ee6e696221ea74dadf772d24f
tymek805 <tymoteusz.lango@op.pl>
2024-05-04 10:27:50 +0200

    Add generator for 7th exercise list

---
+++ src/Semestr_4/ScriptingLanguages/List_7/generator.py
@@ -0,0 +1,23 @@
+def make_generator(function):
+    def generator():
+        n = 1
+        while True:
+            yield function(n)
+            n += 1
+    return generator
+
+
+def fibonacci(n: int):
+    array = [0, 1]
+    if n < 0:
+        raise ValueError("n must be a positive number")
+    elif n < 2:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+if __name__ == "__main__":
+    # print("Generator with Fibonacci: ")
+    # [print(value) for value in make_generator(fibonacci)()]
+    print("Power sequence generator: ")
+    [print(value) for value in make_generator(lambda x: x ** 2)()]
