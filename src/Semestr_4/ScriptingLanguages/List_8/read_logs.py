67bbcda5098575eb6f5ee3f9f8e2317afb3d2355
tymek805 <tymoteusz.lango@op.pl>
2024-05-13 16:44:57 +0200

    Rollback to previous version

--- src/Semestr_4/ScriptingLanguages/List_8/read_logs.py
+++ src/Semestr_4/ScriptingLanguages/List_8/read_logs.py
@@ -8,12 +8,12 @@ def read_logs(path: str) -> list[str]:
 
 def log_to_dict(log: str) -> dict:
     separated_line = log.split()
-    date = datetime.strptime(log.split('[')[1].split(']')[0], '%d/%b/%Y:%H:%M:%S %z')
+    date, timezone = log.split('[')[1].split(']')[0].split()
     method, resource = log.split('"')[1].split()[:2]
     return {
         'host': separated_line[0],
-        'date': date,
-        'timezone': datetime.strftime(date, '%z'),
+        'date': datetime.strptime(date, '%d/%b/%Y:%H:%M:%S'),
+        'timezone': timezone,
         'method': method,
         'resource': resource,
         'code': separated_line[-2],
