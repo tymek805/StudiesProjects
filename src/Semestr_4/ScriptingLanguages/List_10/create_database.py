02824060f68a3fe0e9fe2b62b339cbd1f05fe43f
tymek805 <tymoteusz.lango@op.pl>
2024-05-26 23:51:44 +0200

    Add global values for table names

--- src/Semestr_4/ScriptingLanguages/List_10/create_database.py
+++ src/Semestr_4/ScriptingLanguages/List_10/create_database.py
@@ -2,15 +2,18 @@ import sqlite3
 import sys
 from typing import Text, Dict
 
+stations_table = 'stations'
+rentals_table = 'rentals'
+
 
 def create_database(db_name: Text = 'rentals_db'):
     if not db_name.endswith('.sqlite'):
         db_name += '.sqlite'
 
     table_names: Dict[Text, Text] = {
-        'stations': "station_id INTEGER PRIMARY KEY AUTOINCREMENT, station_name TEXT",
-        'rentals': "rental_id INTEGER PRIMARY KEY, bike_id INTEGER, start_time DATE, end_time DATE, rental_station INTEGER, return_station INTEGER, rental_time INTEGER, "
-                   "FOREIGN KEY (rental_station) REFERENCES stations(station_id), FOREIGN KEY (return_station) REFERENCES stations(station_id)",
+        stations_table: "station_id INTEGER PRIMARY KEY AUTOINCREMENT, station_name TEXT",
+        rentals_table: "rental_id INTEGER PRIMARY KEY, bike_id INTEGER, start_time DATE, end_time DATE, rental_station INTEGER, return_station INTEGER, rental_time INTEGER, "
+                       "FOREIGN KEY (rental_station) REFERENCES stations(station_id), FOREIGN KEY (return_station) REFERENCES stations(station_id)",
     }
 
     if len(sys.argv) == 2:
