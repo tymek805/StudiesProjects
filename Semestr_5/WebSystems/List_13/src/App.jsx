d2534bce797ffb0da3db050b0460ef68b87906e9
Tymoteusz Lango <58483383+tymek805@users.noreply.github.com>
2025-01-29 09:52:48 +0100

    Add List 13

---
+++ Semestr_5/WebSystems/List_13/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Head from './Head'
+import Foot from './Foot'
+import Listazadan from './Listazadan'
+import Form from './Form'
+
+function App() {
+  return (
+    <>
+      <Head/>
+      <Listazadan/>
+      <Foot/>
+      <Form/>
+    </>
+  )
+}
+
+export default App
