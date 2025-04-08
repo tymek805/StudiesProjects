6db15a7f909e8c11b45e30d6f403225e8f96b450
Tymoteusz Lango <58483383+tymek805@users.noreply.github.com>
2025-04-08 14:36:23 +0200

    Refactor

--- Semestr_6/AdvancedWebSystems/Books/books-vue/node_modules/bootstrap-vue-next/README.md
+++
@@ -1,3 +0,0 @@
-# bootstrap-vue-next
-
-This is the root for the bootstrap-vue-next package
--- Semestr_6/AdvancedWebSystems/Books/books-vue/node_modules/vue-good-table-next/src/index.js
+++
@@ -1,9 +0,0 @@
-import VueGoodTable from './components/Table.vue';
-
-export default {
-  install: (app, options) => {
-    app.component('VueGoodTable', VueGoodTable);
-  }
-}
-
-export { VueGoodTable };
