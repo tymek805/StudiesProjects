3417be7a7d114096e47d816ec714665683b660ee
Tymoteusz Lango <58483383+tymek805@users.noreply.github.com>
2025-05-25 20:34:29 +0200

    Small changes

--- Semestr_6/Graphics/List_3/src/Main.java
+++ Semestr_6/Graphics/List_3/src/Main.java
@@ -23,6 +23,7 @@ public class Main extends JPanel {
     }
 
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale", "2.0");
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Paint");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
